d37c719bafaa852fe5924b0516bba4c65c1135e9
KismatMudvari <kismatm31@gmail.com>
2024-11-24 20:38:04 +0545

    TRy

---
+++ hello.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int A = sc.nextInt();
+        Scanner sc= new scanner(System.in);
+        int B = sc.nextInt();
+        
+        Scanner sc= new scanner(System.in);
+        int sum= sc.nextInt();
+
+        System.out.println("Enter A ");
+        System.out.println( "Enter B");
+        System.out.println(Sum="A+B");
+
+    }
+    
+}
