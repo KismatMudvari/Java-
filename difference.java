8c8f0fecd63d5ee4db09d27004ee3cb9c8d4d34d
KismatMudvari <kismatm31@gmail.com>
2024-11-24 19:30:49 +0545

    diff

---
+++ difference.java
@@ -0,0 +1,10 @@
+public class difference {
+    
+    public static void main(String[] args) {
+        int a=10;
+        int b=40;
+        int difference= a- b;
+        System.out.println(difference);
+        
+    }
+}
